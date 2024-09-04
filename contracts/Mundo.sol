120c97d88c87521507de0f7366b34496a32b6137
Muhindo-Galien <muhindogalien512@gmail.com>
2024-09-04 02:18:22 -0700

    Mundo init

---
+++ contracts/Mundo.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+interface IERC20Token {
+  function transfer(address, uint256) external returns (bool);
+  function approve(address, uint256) external returns (bool);
+  function transferFrom(address, address, uint256) external returns (bool);
+  function totalSupply() external view returns (uint256);
+  function balanceOf(address) external view returns (uint256);
+  function allowance(address, address) external view returns (uint256);
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract Mundo {
+    address public owner;
+    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+        string description;
+    }
+
+    struct Order {
+        uint256 time;
+        Item item;
+    }
+
+    mapping(uint256 => Item) public items;
+    mapping(address => mapping(uint256 => Order)) public orders;
+    mapping(address => uint256) public orderCount;
+
+    event Buy(address buyer, uint256 orderId, uint256 itemId);
+    event List(string name, uint256 cost, uint256 quantity);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock,
+        string memory _description
+    ) public onlyOwner {
+        // Create Item
+        Item memory item = Item(
+            _id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock,
+            _description
+        );
+
+        // Add Item to mapping
+        items[_id] = item;
+
+        // Emit event
+        emit List(_name, _cost, _stock);
+    }
+
+    function buy(uint256 _id) public payable {
+        // Fetch item
+        Item memory item = items[_id];
+
+        // Require enough ether to buy item
+        require(msg.value >= item.cost);
+
+        // Require item is in stock
+        require(item.stock > 0);
+
+        // Create order
+        Order memory order = Order(block.timestamp, item);
+
+        // Add order for user
+        orderCount[msg.sender]++; // <-- Order ID
+        orders[msg.sender][orderCount[msg.sender]] = order;
+
+        // Subtract stock
+        items[_id].stock = item.stock - 1;
+
+        // Pay order
+        require(
+		  IERC20Token(cUsdTokenAddress).transferFrom(
+			msg.sender,
+			owner,
+			item.cost
+		  ),
+		  "Transfer failed."
+		);
+
+        // Emit event
+        emit Buy(msg.sender, orderCount[msg.sender], item.id);
+    }
+
+    // getters function
+    function getItem(uint256 itemId)
+        external
+        view
+        returns (Item memory)
+    {
+        return items[itemId];
+    }
+}
